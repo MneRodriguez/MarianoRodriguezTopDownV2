e9de217edd0a66799cda77e92bf8ea6b365397cb
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-05-28 18:35:07 -0300

    Initial commit

---
+++ Assets/Scripts/ControlCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCam : MonoBehaviour
+{
+    public GameObject Jugador;
+    public Vector2 offset;
+    public float movtoCam = 0.5f;
+
+    void Start()
+    {
+        offset = transform.position + Jugador.transform.position;
+    }
+
+    
+    void Update()
+    {
+        Vector2 posDeCam = Jugador.transform.TransformPoint(new Vector3(-1f, -1f, -10f));
+        transform.position = Vector2.SmoothDamp(transform.position, posDeCam, ref offset, movtoCam);
+    }
+}
---
+++ Assets/Scripts/ControlJgdr.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJgdr : MonoBehaviour
+{
+    public Camera cam;
+
+    public Rigidbody2D rbJgdr;
+    public float velMovto = 10f;
+
+    void Start()
+    {
+        rbJgdr = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        pos.x += Input.GetAxisRaw("Horizontal") * velMovto * Time.deltaTime;
+        pos.y += Input.GetAxisRaw("Vertical") * velMovto * Time.deltaTime;
+
+        transform.position = pos;
+
+        var posDeCam = Camera.main.transform.TransformPoint(transform.position);
+        var direccion = Input.mousePosition - posDeCam;
+        float angulo = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angulo, Vector3.forward);
+    }
+}
