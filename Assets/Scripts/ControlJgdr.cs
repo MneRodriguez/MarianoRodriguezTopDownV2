b9ad387b6809102a4f1ef9486eec144b863cb086
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-05-30 15:17:17 -0300

    Rotacion 8 direcciones y disparo listo (no es con pooling)

--- Assets/Scripts/ControlJgdr.cs
+++ Assets/Scripts/ControlJgdr.cs
@@ -22,16 +22,18 @@ public class ControlJgdr : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 pos = transform.position;
+        // Movimiento tradicional del player con WASD
+        Vector2 pos = transform.position; 
 
         pos.x += Input.GetAxisRaw("Horizontal") * velMovto * Time.deltaTime;
         pos.y += Input.GetAxisRaw("Vertical") * velMovto * Time.deltaTime;
 
         transform.position = pos;
 
-        var posDeCam = Camera.main.transform.TransformPoint(transform.position);
+        // Movimiento 8 direcciones del player con el MOUSE
+        var posDeCam = Camera.main.WorldToScreenPoint(transform.position);
         var direccion = Input.mousePosition - posDeCam;
-        float angulo = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
+        var angulo = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angulo, Vector3.forward);
     }
 }
---
+++ Assets/Scripts/DisparoJgdr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparoJgdr : MonoBehaviour
+{
+    public Transform zonaSpawnDelDisparo;
+    public GameObject prefabBalaJgdr;
+
+    public float velDesplzBalaJgdr = 20f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Disparar();
+        }
+    }
+
+    void Disparar()
+    {
+        // ESTA FUNCION DEBE TOMAR LA CANT DE BALAS GENERADAS EN EL POOLING -TODAVIA NO SE COMO-
+
+        GameObject balaJgdr = Instantiate(prefabBalaJgdr, zonaSpawnDelDisparo.position, zonaSpawnDelDisparo.rotation);
+        Rigidbody2D rbBala = balaJgdr.GetComponent<Rigidbody2D>();
+        rbBala.AddForce(zonaSpawnDelDisparo.right * velDesplzBalaJgdr, ForceMode2D.Impulse);
+
+        //Instantiate(prefabBalaJgdr, zonaSpawnDelDisparo.position, zonaSpawnDelDisparo.rotation);
+    }
+}
